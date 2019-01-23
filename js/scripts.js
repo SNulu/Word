8246bc63c15e1317784154f8d1b5269ed5fe0715
Riley Benson and Sanyu Prado <riley@>
2019-01-23 15:01:28 -0800

    make results show up on html instead of alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
-var input = prompt("Enter a sentence");
-var newArr = []
-
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+var input = $("#sentence").val();
+var newArr = [];
+debugger;
 for (var i = 0; i < input.length; i++)
+{
 if (input[i] === "a" || input[i] === "e" || input[i] === "i" || input[i] === "o" || input[i] === "u"){
  newArr.push("-");
 } else {
 	newArr.push(input[i]);
 }
+}
 var final = newArr.join("");
-alert(final);
+$("#display").text(final).val();
+
+  event.preventDefault();
+});
+});
